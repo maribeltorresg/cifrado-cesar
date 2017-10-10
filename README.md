c85b8e7192c0f148f7331c98cf913d288a74b2a7
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-10-10 17:26:47 -0500

    Implementación decipher

---
+++ README.md
@@ -0,0 +1,16 @@
+Inicio
+Titulo: cifrado-Cesar
+
+Preguntar al usuario por medio de un prompt
+
+Crear una variable para almacenar un mensaje
+
+Por medio de un for iteramos
+    para i <-- x hasta n a incrementado de s hacer
+    instrucciones
+fin para
+
+Fin
+
+
+
--- js/app.js
+++ js/app.js
@@ -15,9 +15,15 @@ function cipher(message){
 }
 var message = prompt('Ingrese una frase');
 alert(cipher(message));
- 
-
-// La formula para descifrar es (x - n) % 26
-function decipher (string){
-   
+function decipher (message){
+   var result = '';
+   for (var i = 0; i < message.length; i++){
+       var charCode = message.charCodeAt(i);
+       var newCode = (charCode - 65 - 33) % 26 + 65;
+       var letter = String.fromCharCode(newCode);
+       result += letter;
+   }
+   return result;
 }
+var message = prompt('Ingrese una frase');
+alert(decipher(message));
